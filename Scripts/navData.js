71fa44b609477fb11a5ab94390dcdfc1afbfd482
JackieRino <jackieosupeng@gmail.com>
2026-08-17 10:08:15 +0100

    Fix relative paths in navData URLs
    Updated URLs in navData to use relative paths.

--- Scripts/navData.js
+++ Scripts/navData.js
@@ -1,5 +1,5 @@
  const navData = [
-     {name:"Home",      url: "/Html/home.html",    icon:false },
-     {name:"New Entry", url:"/Html/newEntry.html", icon:false}, 
-     {name: "Vault",    url:"/Html/vault.html",    icon:false},
-     {name:"Profile",   url:"/Html/profile.html",  icon:true, iconName: "fa-regular fa-circle-user"}];
+     {name:"Home",      url: "../Html/home.html",    icon:false },
+     {name:"New Entry", url:"..//Html/newEntry.html", icon:false}, 
+     {name: "Vault",    url:"..//Html/vault.html",    icon:false},
+     {name:"Profile",   url:"..//Html/profile.html",  icon:true, iconName: "fa-regular fa-circle-user"}];
